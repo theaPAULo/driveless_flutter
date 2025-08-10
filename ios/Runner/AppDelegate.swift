7fbc5c6b7dfb89e54183782b0fdbd69af3e4ef18
Paul <psoni511@gmail.com>
2025-08-10 00:42:31 -0500

    apple maps and iOS working midnight commit pt2

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,16 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Initialize Google Maps with your API key
+    GMSServices.provideAPIKey("AIzaSyCancy_vwbDbYZavxDjtpL7NW4lYl8Tkmk")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
